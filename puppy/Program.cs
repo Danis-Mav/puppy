13d925b1ccd25e342943991daa45d063a9c656b4
Danis Mavlin <mavlindanis@gmail.com>
2021-06-09 12:50:38 +0300

    Добавьте файлы проекта.

---
+++ puppy/Program.cs
@@ -0,0 +1,56 @@
+﻿// OpenWeatherMap REST API example
+// DKY 2021
+
+using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace OpenWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = $"http://www.recipepuppy.com/api/";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+               var weatherForecast = JsonConvert.DeserializeObject<Root>(result);
+               Console.WriteLine(weatherForecast.results);
+            }
+
+        }
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Result
+    {
+        public string title { get; set; }
+        public string href { get; set; }
+        public string ingredients { get; set; }
+        public string thumbnail { get; set; }
+    }
+
+    public class Root
+    {
+        public string title { get; set; }
+        public double version { get; set; }
+        public string href { get; set; }
+        public List<Result> results { get; set; }
+    }
+}
---
+++ puppy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("puppy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("puppy")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7a5a6af2-5032-4583-bf7b-22bd9af4e236")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
